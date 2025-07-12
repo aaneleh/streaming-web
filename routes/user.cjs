30033166fdd2ee40e3295cc5783d525db8908602
aaneleh <helenakurzgon@gmail.com>
2025-07-12 00:16:07 -0300

    feat: login e CRUD usuários

--- routes/user.cjs
+++ routes/user.cjs
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const mysql = require('mysql2')
+const CryptoJS = require("crypto-js")
+const { v4: uuidv4 } = require('uuid')
+const { redirect } = require('react-router')
 
 const connection = mysql.createConnection({
     host: process.env.HOST,
@@ -8,32 +11,104 @@ const connection = mysql.createConnection({
     password: process.env.PASSWORD,
     database: process.env.DATABASE
 })
-connection.connect();
-
-/*
-connection.query('SELECT * FROM user', function (error, results, fields) {
-  if (error) throw error;
-  for(let i = 0; i < results.length; i++) {
-      console.log(results[i]);
-  }
-});
-
- connection.query('INSERT INTO user VALUES (0, "Helena", "12345", 0)', function (error, results, fields) {
-  if (error) throw error;
-}); */
-
+connection.connect()
 
 //SELECIONA TODOS
 router.get('/', async(req, res) => {
+
     connection.query('SELECT * FROM user', function (error, results, fields) {
         if (error) {
-            res.status(500).json({message: error})
-            throw error;  
+            console.log(error)
+            return res.status(500).json({message: 'Erro listando usuários'})
         } 
-        res.json(results)
-    });
+        return res.status(200).json(results)
+    })
 })
 
-/* connection.end(); */
+//CRIA USUÁRIO
+router.post('/', async(req, res) => {
+
+    let user_name = req.body.user_name
+    let password = CryptoJS.AES.encrypt(req.body.password, process.env.CRYPTO_KEY).toString()
+    let type = req.body.type
+
+    connection.query('INSERT INTO user VALUES (?, ?, ?, ?)', [uuidv4(), user_name, password, type], function (error, results, fields) {
+        if (error) {
+            console.log(error)
+            if(error.code == 'ER_DUP_ENTRY'){
+                return res.status(400).json({message: 'Nome de usuário já existe'})
+            }
+            return res.status(500).json({message: 'Erro ao cadastrar usuário'})
+        } 
+        return res.status(200).json({message: 'Usuário cadastrado com sucesso'})
+    }) 
+})
+
+//EDITA NOME
+router.patch('/:id', async(req, res) => {
+
+    let user_id = req.params.id
+    let user_name = req.body.user_name
+
+    if(user_name == null) res.status(400)
+
+    connection.query('UPDATE user SET name = ? WHERE user_id = ?', [user_name, user_id], function (error, results, fields) {
+        if (error) {
+            console.log(error)
+            return res.status(500).json({message: 'Erro alterando usuário'})
+        } 
+        return res.status(200).json({message: 'Erro alterando usuário'})
+    }) 
+})
+
+//EDITA SENHA
+router.patch('/password/:id', async(req, res) => {
+
+    let user_id = req.params.id
+    let current_password = req.body.current_password
+    let new_password = req.new_password
+    
+    connection.query('SELECT * FROM user WHERE user_id = ?', [user_id], function (error, results, fields) {
+
+        if (error) {
+            console.log(error)
+            return res.status(500).json({message: error})
+        } 
+        if (results.length == 0) {
+            return res.status(404).json({message: 'Usuário não existe'})
+        }
+        
+        let password = results[0].password
+        var decrypto = CryptoJS.AES.decrypt(password, process.env.CRYPTO_KEY).toString(CryptoJS.enc.Utf8)
+
+        if(decrypto === current_password){
+
+            connection.query('UPDATE user SET password = ? WHERE user_id = ?', [CryptoJS.AES.encrypt(new_password, process.env.CRYPTO_KEY).toString(), user_id], function (error, results, fields) {
+                if(error) {
+                    console.log(error)
+                    return res.status(500).json({message: 'Erro ao alterar senha'})
+                }
+                return res.status(200).json({message: 'Senha alterada'})
+            })
+
+        } else {
+            return res.status(400).json({message: 'Senha incorreta'})
+        }
+    }) 
+})
+
+//EXCLUIR USUÁRIO
+router.delete('/:id', async(req, res) => {
+
+    let user_id = req.params.id
+
+    connection.query('DELETE FROM user WHERE user_id = ?', [user_id], function (error, results, fields) {
+        if (error) {
+            console.log(error)
+            return res.status(500).json({message: 'Erro ao excluir o usuário'})
+        } 
+        return res.status(200).json({message: 'Usuário excluído com sucesso'})
+    }) 
+})
 
 module.exports = router
--- server.cjs
+++ server.cjs
@@ -1,6 +1,8 @@
 require('dotenv').config()
 const express = require('express')
-const cors = require('cors');
+const cors = require('cors')
+const mysql = require('mysql2')
+const CryptoJS = require("crypto-js")
 const userRouter = require('./routes/user.cjs')
 const movieRouter = require('./routes/movie.cjs')
 
@@ -9,6 +11,43 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+
+
+const connection = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
+})
+connection.connect()
+
+//LOGIN
+app.post('/login', async(req, res) => {
+
+    let user_name = req.body.user_name
+    let passwordBody = req.body.password
+    
+    connection.query('SELECT * FROM user WHERE name = ?', [user_name], function (error, results, fields) {
+
+        if (error) {
+            console.log(error)
+            return res.status(500).json({message: error})
+        } 
+        if (results.length == 0) {
+            return res.status(404).json({message: 'Usuário não existe'})
+        }
+        
+        let password = results[0].password
+        var decrypto = CryptoJS.AES.decrypt(password, process.env.CRYPTO_KEY).toString(CryptoJS.enc.Utf8)
+
+        if(decrypto === passwordBody){
+            return res.status(200).json({message: 'Login realizado com sucesso'})
+
+        } else {
+            return res.status(400).json({message: 'Senha incorreta'})
+        }
+    })
+})
 
 app.use('/user', userRouter)
 app.use('/movie', movieRouter)
